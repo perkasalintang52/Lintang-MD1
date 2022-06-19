acec27d7f5f43b3f79dae486e06e9cbb9d0cde8e
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-06-19 18:57:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6281322577123'],
+  ['6281322577123'],
+  ['6281322577123', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6281322577123'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© urfavboy-lintang'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
